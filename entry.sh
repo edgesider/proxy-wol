358fd2968054c396a18646396a255b3b738f8c11
ykai <me@ykai.cc>
2025-01-20 01:57:07 +0800

    agent port error

--- entry.sh
+++ entry.sh
@@ -3,8 +3,8 @@
 PORT=${PORT:-4321}
 TARGET_HOST=${TARGET_HOST:?TARGET_HOST is not set}
 TARGET_PORT=${TARGET_PORT:?TARGET_PORT is not set}
+TARGET_AGENT_PORT=${TARGET_AGENT_PORT:?TARGET_AGENT_PORT is not set}
 TARGET_MAC=${TARGET_MAC:?TARGET_MAC is not set}
-TARGET_SSH_CMD=${TARGET_SSH_CMD:?TARGET_SSH_CMD is not set}
 
 cmd="gunicorn proxy:main --worker-class aiohttp.GunicornWebWorker -b 0.0.0.0:$PORT --reuse-port --capture-output --access-logfile -"
 echo $cmd
