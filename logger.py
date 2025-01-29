7cb797238b4d64f5e263e55a0c673fa8536a13ae
ykai <me@ykai.cc>
2025-01-29 12:47:28 +0800

    支持DEBUG env

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG if os.environ.get('DEBUG') else logging.INFO)
 logger = logging.getLogger('proxy-wol')
